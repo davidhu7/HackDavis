ec0dcb294cf7eecdc130c7938723b88da9e1e2ca
ksong200 <hud1945@gmail.com>
2021-01-16 17:03:06 -0500

    idk

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request, make_response, send_from_directory
 
 app = Flask(__name__)
 
 @app.route('/')
 def index(name=None):
     return render_template('index.html', name=name)
+
+app.run()
